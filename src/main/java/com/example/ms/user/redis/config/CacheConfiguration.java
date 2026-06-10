de4ce04307c765af5ee9166f1ee194bbbc14b66f
farid.mustafayev <farid.mustafayev@cybernet.az>
2026-06-10 22:14:06 +0400

    To read data from redis and return it in json format

--- src/main/java/com/example/ms/user/redis/config/CacheConfiguration.java
+++ src/main/java/com/example/ms/user/redis/config/CacheConfiguration.java
@@ -1,8 +1,12 @@
 package com.example.ms.user.redis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.experimental.FieldDefaults;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.codec.SerializationCodec;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,13 +21,28 @@ public class CacheConfiguration {
     @Value("${redis.server.url}")
     String redisServer;
 
+//    @Bean
+//    public RedissonClient redissonSingleClient() {
+//        Config config = new Config();
+//        config
+//                .setCodec(new SerializationCodec())
+//                .useSingleServer()
+//                .setAddress(redisServer);
+//        return Redisson.create(config);
+//    }
+
     @Bean
-    public RedissonClient redissonSingleClient() {
+    public RedissonClient redissonClient() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         Config config = new Config();
-        config
-                .setCodec(new SerializationCodec())
-                .useSingleServer()
+        config.setCodec(new JsonJacksonCodec(objectMapper));
+
+        config.useSingleServer()
                 .setAddress(redisServer);
+
         return Redisson.create(config);
     }
 }
